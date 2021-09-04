765d62079f340299ba7f96da5f8665e726ce702f
Gino Messmer <gino@messmerized.de>
2021-09-04 20:12:25 +0200

    Create subscription

--- ProjectAmy.ClientWorker/InitializerWorker.cs
+++ ProjectAmy.ClientWorker/InitializerWorker.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Graph;
 using Microsoft.Identity.Client;
 
 namespace ProjectAmy.ClientWorker
@@ -13,11 +14,14 @@ namespace ProjectAmy.ClientWorker
     {
         private readonly ILogger<InitializerWorker> _logger;
         private readonly IPublicClientApplication _app;
+        private readonly GraphServiceClient _graphServiceClient;
 
-        public InitializerWorker(ILogger<InitializerWorker> logger, IPublicClientApplication app)
+        public InitializerWorker(ILogger<InitializerWorker> logger,
+            IPublicClientApplication app, GraphServiceClient graphServiceClient)
         {
             _logger = logger;
             _app = app;
+            _graphServiceClient = graphServiceClient;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -27,15 +31,55 @@ namespace ProjectAmy.ClientWorker
             if (!accounts.Any())
             {
                 // Sign in
-                var result = await _app.AcquireTokenInteractive(new[] {"ChannelMessage.Read.All"})
+                var result = await _app.AcquireTokenInteractive(Program.Scopes)
                     .ExecuteAsync(stoppingToken);
 
                 _logger.LogInformation("Sign-in successful!");
             }
 
             // Check subscriptions
+            try
+            {
+                var subscriptions = await _graphServiceClient.Subscriptions.Request()
+                    //.Filter($"startsWith(notificationUrl,'{Program.FunctionsEndpoint}')")
+                    .GetAsync(stoppingToken);
 
-            // Register subscription if required
+                _logger.LogInformation("Found {subscriptions}", subscriptions.Count);
+
+                if (!subscriptions.Any())
+                {
+                    await CreateSubscriptionAsync(stoppingToken);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to retrieve subscriptions");
+            }
+        }
+
+        private async Task CreateSubscriptionAsync(CancellationToken stoppingToken)
+        {
+            try
+            {
+                // Register subscription if required
+                var subscription = new Subscription
+                {
+                    ChangeType = "created",
+                    NotificationUrl = Program.FunctionsEndpoint,
+                    Resource = $"/teams/{Program.TeamId}/channels/{Program.ChannelId}/messages",
+                    ExpirationDateTime = DateTime.UtcNow + TimeSpan.FromDays(30),
+                    //ClientState = "secretClientValue",
+                    LatestSupportedTlsVersion = "v1_2"
+                };
+
+                await _graphServiceClient.Subscriptions
+                    .Request()
+                    .AddAsync(subscription, stoppingToken);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Failed to create a subscription");
+            }
         }
     }
 }
--- ProjectAmy.ClientWorker/Program.cs
+++ ProjectAmy.ClientWorker/Program.cs
@@ -3,13 +3,23 @@ using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Microsoft.Graph;
 using Microsoft.Identity.Client;
 
 namespace ProjectAmy.ClientWorker
 {
     public class Program
     {
+
+        public static IEnumerable<string> Scopes = new[] { "ChannelMessage.Read.All" };
+
+        public const string FunctionsEndpoint = "https://example.com";
+        public const string TeamId = "1047af6d-3a4b-4270-97c5-8694e61582e5";
+        public const string ChannelId = "19:ozBKIey0-SsHoj8F9f2-U6cKbdZ3FOHxupLR2kDIxww1@thread.tacv2";
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
@@ -23,6 +33,16 @@ namespace ProjectAmy.ClientWorker
                         .Create("00f05160-5e0b-4645-ab5f-f35797d95168")
                         .WithDefaultRedirectUri()
                         .Build());
+
+                    services.AddTransient(sp => new GraphServiceClient(new DelegateAuthenticationProvider(
+                        async request =>
+                        {
+                            var app = sp.GetRequiredService<IPublicClientApplication>();
+                            var account = (await app.GetAccountsAsync()).FirstOrDefault();
+                            var result = await app.AcquireTokenSilent(Scopes, account).ExecuteAsync();
+                            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", result.AccessToken);
+                        })));
+
                     services.AddHostedService<InitializerWorker>();
                 });
     }
